24cd893a7ec52e5e3b96798ec56688ac90d25b3f
vzhirnov <kraster85@gmail.com>
2022-03-25 09:37:45 -0700

    small refactoring

--- post_json_fuzzer.py
+++ post_json_fuzzer.py
@@ -13,11 +13,6 @@ from src.core.fuzz_data_creators import get_jsons_for_fuzzing
 # TODO: make interactive work with 500 responses:
 #  repeat 500th requests until all random 500th answers turn into 200th answers
 
-# TODO agrparse
-# json file with json bodies and tules
-# url to send
-# additional headers, if required
-
 url = str()
 headers = dict()
 file = str()
@@ -68,52 +63,6 @@ if args.file:
     file = args.file
 
 
-d_base = {
-    "clientid": (132, 'STRATEGY_1'),
-    "type": ("disable_attack_type", 'STRATEGY_2'),
-    # "attack_type": (["xss"], 'STAR'),
-    # "point": ("action_ext_1", 'STRATEGY_1', "[]^[]^", "action_ext_2", 'STRATEGY_2', "[]^", "action_ext_star", 'STAR', '++'),
-    # "dict_field": ({"point": ["path", 0], "type": "absent"}, 'STAR'),
-    # "validated": (True, False),
-
-    # ("action", "another_action"):  # (0, 1, True)
-    #     [
-    #         {("point", '*'): ["header", "HOST"], "type": ("iequal", "equal", "absent"), "value": ("testcom", '*')},
-    #         {"point": ["path", 0], "type": "absent"},
-    #         {"point": ["action_name"], "type": "equal", "value": ""},
-    #         {"point": ["action_ext"], "type": "absent"}
-    #     ],
-    # "token": "1b64c60e7d3e5cdabd63ba61f6e997ee",
-}
-
-
-# d_base = {
-#     "clientid":(132, 'STRATEGY_1'),
-#     "type":"disable_attack_type",
-#     "attack_type":"sqli",
-#     "point":[["action_ext"]],
-#     "validated":False,
-#     "action":
-#         [
-#             {"point":["header","HOST"],"type":"iequal","value":"test.com"},
-#             {"point":["path",0],"type":("equal", None, 1, "1"),"value":"1"},
-#             {"point":["path",1],"type":"equal","value":"2"},
-#             {"point":["path",2],"type":"equal","value":"3"},
-#             {"point":["path",3],"type":"absent"},
-#             {"point":["action_name"],"type":"equal","value":"index"},
-#             {"point":["action_ext"],"type":"equal","value":"php"}
-#         ],
-#     "group_uuid":"26797500-9733-40c4-b4fe-6c47d56c647d",
-#     "group_action":"create",
-#     "token":"8417d67f4737a93970c40dc3e6251d65",
-#
-# }
-
-# headers = {
-#     "X-WallarmAPI-UUID": "a5ed3d6a-aeba-43ad-8f9c-431bf558dbb9",
-#     "X-WallarmAPI-Secret": "65a31fd20b35bba95a4a1e1ba92d0dba73eddf5d46e3792a6ac965a1f28f3282"
-# }
-
 async def post(url_aim, json_params, hdrs, request_metainfo):
     async with aiohttp.ClientSession() as session:
         async with session.post(url_aim, json=json_params, headers=hdrs, ssl=False) as response:
@@ -151,10 +100,7 @@ if __name__ == '__main__':
             employee_writer.writerow(l)
 
 # TODO known issues:
-# test.com is splitted into ['test', 'com'] - DONE
-# wrong handle of -1 - numbers with minus - DONE
 # wrong handle of "validated":("", 'STRATEGY_1'), - empty field - DESICION - use '\0' - DONE???
-# wrong handle of "validated":(111, 222, 'STRATEGY_1') - several items plus strategy - DONE
 # cannot make case with lack of current parameter AND doubled string
 # "value":("test.com") - one value handles incorrectly
 # if basic json has no any ()s, -> error
