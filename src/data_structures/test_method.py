cfa614f23e869d390ca7cf2ecac559d364482991
vzhirnov <kraster85@gmail.com>
2022-04-28 20:43:41 -0700

    Add some test methods for future

--- src/data_structures/test_method.py
+++ src/data_structures/test_method.py
@@ -3,7 +3,10 @@ import enum
 
 @enum.unique
 class TestMethod(enum.Enum):
-    hypothesis = 6
+    miss_it = 9
+    add_several_times = 8
+    nothing_more_but_this = 7
+    hypothesis = 6  # ?
     pair_wise = 5
     triple_wise = 4
     combinations = 3
