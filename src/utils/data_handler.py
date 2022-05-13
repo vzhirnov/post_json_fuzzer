13a54a0a773e247b697fc954793e2b925403be6e
vzhirnov <kraster85@gmail.com>
2022-05-13 00:38:29 -0700

    Add separate results folder for every deck

--- src/utils/data_handler.py
+++ src/utils/data_handler.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from datetime import datetime
 from default_values import DefaultValues
 
-from src.utils.files_handler import make_dir
+from src.utils.files_handler import make_dir, get_filename
 
 
 def save_general_log():
@@ -20,6 +20,10 @@ def save_artifacts_to_corr_files(actual_results: dict):
 
     for deck_name, result_for_separate_deck in actual_results.items():
         all_reply_statuses = {}
+        deck_file_name = get_filename(deck_name)
+        path_to_deck_folder = Path(dir_name_with_curr_date_time / deck_file_name)
+        if not make_dir(str(path_to_deck_folder)):
+            raise Exception(f"Error: cannot create results directories")
         for artifacts in result_for_separate_deck:
             if all_reply_statuses.get(artifacts[0].status, None):
                 all_reply_statuses[artifacts[0].status] = all_reply_statuses[
@@ -30,7 +34,7 @@ def save_artifacts_to_corr_files(actual_results: dict):
 
         for status, response_data in all_reply_statuses.items():
             with open(
-                Path(dir_name_with_curr_date_time / str(status)), mode="w"
+                Path(path_to_deck_folder / str(status)), mode="w"
             ) as file:
                 for data in response_data:
                     file.write(f"{data}\n")
