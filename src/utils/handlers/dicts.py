e799c1e0eede8c02127074ffdd0b0bf235c55cc3
vzhirnov <kraster85@gmail.com>
2022-07-29 03:45:44 -0700

    WIP: refactor fuzzies indexation: add DLContainer

--- src/utils/handlers/dicts.py
+++ src/utils/handlers/dicts.py
@@ -1,6 +1,7 @@
 import ast
+import inspect
 
-from typing import Union, List, Dict
+from typing import Union, List, Dict, Any
 
 from copy import copy
 from src.datastructures.fuzzy import Fuzzy
@@ -121,3 +122,183 @@ def make_dictionary_items_unique(jsons: list):
     for item in res_set:
         res_list.append(ast.literal_eval(item))
     return res_list
+
+
+class DLContainer:
+    def __init__(self, base_object: dict):
+        self.base_object = base_object
+        self.base_object_name = self.retrieve_arg_name(base_object).pop()
+        self.result = []
+        self.path = []
+
+    def retrieve_arg_name(self, var: Any) -> list:
+        """
+        Get var's name.
+        See https://stackoverflow.com/questions/18425225/getting-the-name-of-a-variable-as-a-string
+        :param var: Any object
+        :return: var name
+        """
+        callers_local_vars = inspect.currentframe().f_back.f_locals.items()
+        return [var_name for var_name, var_val in callers_local_vars if var_val is var]
+
+    def retrieve_arg_real_name(self, var: Any) -> list:
+        """
+        Get var's real name.
+        See https://stackoverflow.com/questions/18425225/getting-the-name-of-a-variable-as-a-string
+        :param var: Any object
+        :return: var name
+        """
+        callers_local_vars = inspect.currentframe().f_back.f_back.f_locals.items()
+        return [var_name for var_name, var_val in callers_local_vars if var_val is var]
+
+    def clean_data(self):
+        self.result.clear()
+        self.path.clear()
+
+    def get(self, key_or_value: Any, default_value: None):
+        """
+        :param key_or_value: key or value to find
+        :param default_value: will return if item is not found
+        :return: path to item, and digit which is either 0(key is found) or 1(value is found)
+        """
+        self.clean_data()
+        self.find_path_for_key(self.base_object, key_or_value)
+        if self.result:
+            res = self.result.pop()
+            return res, 0
+        else:
+            self.clean_data()
+            self.find_path_for_value(self.base_object, key_or_value)
+            if self.result:
+                res = self.result.pop()
+                return res, 1
+        return default_value, None
+
+    def find_path_for_key(self, dict_obj: dict, key: Any, i=None):  # TODO make method private
+        for k, v in dict_obj.items():
+            self.path.append(k)
+            if isinstance(v, dict):
+                self.find_path_for_key(v, key, i)
+            if isinstance(v, list):
+                for i, item in enumerate(v):
+                    self.path.append(i)
+                    if isinstance(item, dict):
+                        self.find_path_for_key(item, key, i)
+                    self.path.pop()
+            if k == key:
+                self.result.append(copy(self.path))
+            if self.path:
+                self.path.pop()
+
+    def find_path_for_value(self, dict_obj: Union[Dict, List], value: Fuzzy, i=None):  # TODO make method private
+        if isinstance(dict_obj, list):
+            for i, item in enumerate(dict_obj):
+                self.path.append(i)
+                if item == value:
+                    self.result.append(copy(self.path))
+                    return
+                if isinstance(item, dict):
+                    self.find_path_for_value(item, value, i)
+                if isinstance(item, list):
+                    self.find_path_for_value(item, value, i)
+                self.path.pop()
+        if isinstance(dict_obj, dict):
+            for k, v in dict_obj.items():
+                self.path.append(k)
+                if isinstance(v, dict):
+                    self.find_path_for_value(v, value, i)
+                if isinstance(v, list):
+                    self.find_path_for_value(v, value, i)
+                if v == value:
+                    self.result.append(copy(self.path))
+                if self.path:
+                    self.path.pop()
+
+    def new_get_access_view_to_deep_key(self, dic_name: str, path: dict):
+        d_name = self.retrieve_arg_real_name(path).pop()
+        res = str(dic_name)
+        max_dict_index = max(list(path.keys()))
+        del path[max_dict_index]
+        for path_index, path_value in path.items():
+            res += "[" + d_name + "[" + str(path_index) + "]" + "]"
+        return res
+
+    def new_get_access_view_to_deep_value(self, dic_name: str, path: dict):
+        d_name = self.retrieve_arg_real_name(path).pop()
+        res = str(dic_name)
+        for path_index, path_value in path.items():
+            res += "[" + d_name + "[" + str(path_index) + "]" + "]"
+        return res
+
+    def set_new_item(self, path: list, new_value: Any):
+        pass
+
+    def update_key(self, path: list, new_value: Any):
+        base_object = self.base_object  # it is need for exec
+        new_path = path
+        path_vars = {}
+        for i, item in enumerate(new_path):
+            path_vars[i] = item
+
+        access_view_to_curr_key = self.new_get_access_view_to_deep_value(
+            self.base_object_name, path_vars
+        )
+
+        obj_to_replace = new_value if isinstance(new_value, str) else "'" + str(new_value) + "'"
+
+        access_view_to_req_key = self.new_get_access_view_to_deep_key(
+            self.base_object_name, path_vars
+        ) + str([obj_to_replace])
+
+        for i, item in enumerate(new_path):
+            path_vars[i] = item
+
+        add_new_kv = "self." + access_view_to_req_key + " = " + "self." + access_view_to_curr_key
+        exec(add_new_kv)
+
+        del_old_kv = "del self." + access_view_to_curr_key
+        exec(del_old_kv)
+        a = 1
+
+    def update_value(self, path: list, new_value: Any):
+        new_path = path
+        path_vars = {}
+        for i, item in enumerate(new_path):
+            path_vars[i] = item
+
+        access_view_to_key = self.new_get_access_view_to_deep_value(
+            self.base_object_name, path_vars
+        )
+
+        obj_to_replace = new_value if not isinstance(new_value, str) else "'" + str(new_value) + "'"
+        base_object = self.base_object
+        add_new_kv = "self." + access_view_to_key + " = " + f"{obj_to_replace}"
+        exec(add_new_kv)
+
+    def update_item(self, path: list, new_value: Any, key_or_value: int):
+        if path is None:
+            return
+        if key_or_value == 0:
+            self.update_key(path, new_value)
+        elif key_or_value == 1:
+            self.update_value(path, new_value)
+        else:
+            return
+
+    def delete_item(self, path: list, item_to_delete: Any):
+        pass
+
+    def find_obj_in_dict_and_replace_it(self, c_obj, obj_to_replace, replacement_obj):
+        if c_obj == obj_to_replace:
+            return replacement_obj
+        elif isinstance(c_obj, list):
+            for i, v in enumerate(c_obj):
+                c_obj[i] = self.find_obj_in_dict_and_replace_it(
+                    v, obj_to_replace, replacement_obj
+                )
+        elif isinstance(c_obj, dict):
+            for k, v in c_obj.items():
+                c_obj[k] = self.find_obj_in_dict_and_replace_it(
+                    v, obj_to_replace, replacement_obj
+                )
+        return c_obj
--- tests/utils/handlers/test_dicts.py
+++ tests/utils/handlers/test_dicts.py
@@ -6,7 +6,6 @@ from src.datastructures.testmethod import TestMethod as tm
 
 
 class TestDicts:
-
     fuzzy = Fuzzy(
         default_value="point",
         suspicious_responses=[403, 200],
@@ -20,14 +19,14 @@ class TestDicts:
             ({fuzzy: "1", "2": 2, "3": 3}, fuzzy, [fuzzy.get_itself()]),
             ({"1": {fuzzy: 1}, "2": 2, "3": 3}, fuzzy, ["1", fuzzy.get_itself()]),
             (
-                {"1": {"4": [{fuzzy: 1}]}, "2": 2, "3": 3},
-                fuzzy,
-                ["1", "4", 0, fuzzy.get_itself()],
+                    {"1": {"4": [{fuzzy: 1}]}, "2": 2, "3": 3},
+                    fuzzy,
+                    ["1", "4", 0, fuzzy.get_itself()],
             ),
             (
-                {"1": {"4": [{"5": [{fuzzy: 6}]}]}, "2": 2, "3": 3},
-                fuzzy,
-                ["1", "4", 0, "5", 0, fuzzy.get_itself()],
+                    {"1": {"4": [{"5": [{fuzzy: 6}]}]}, "2": 2, "3": 3},
+                    fuzzy,
+                    ["1", "4", 0, "5", 0, fuzzy.get_itself()],
             ),
         ],
     )
@@ -43,24 +42,24 @@ class TestDicts:
             ({"1": {1: fuzzy}, "2": 2, "3": 3}, fuzzy, ["1", 1]),
             ({"1": {"4": [{1: fuzzy}]}, "2": 2, "3": 3}, fuzzy, ["1", "4", 0, 1]),
             (
-                {"1": {"4": [{"5": [{6: fuzzy}]}]}, "2": 2, "3": 3},
-                fuzzy,
-                ["1", "4", 0, "5", 0, 6],
+                    {"1": {"4": [{"5": [{6: fuzzy}]}]}, "2": 2, "3": 3},
+                    fuzzy,
+                    ["1", "4", 0, "5", 0, 6],
             ),
             (
-                {'1': [fuzzy, 'other_str']},
-                fuzzy,
-                ['1', 0]
+                    {'1': [fuzzy, 'other_str']},
+                    fuzzy,
+                    ['1', 0]
             ),
             (
-                {'1': [[fuzzy, 'other_str']]},
-                fuzzy,
-                ['1', 0, 0]
+                    {'1': [[fuzzy, 'other_str']]},
+                    fuzzy,
+                    ['1', 0, 0]
             ),
             (
-                {'1': {2: [fuzzy, 'other_str']}},
-                fuzzy,
-                ['1', 2, 0]
+                    {'1': {2: [fuzzy, 'other_str']}},
+                    fuzzy,
+                    ['1', 2, 0]
             ),
         ],
     )
@@ -89,30 +88,30 @@ class TestDicts:
             (["b", "a", "d", "c"], ["a", "b", "c", "d"]),
             (["b", "a", ["d", "c"]], [["c", "d"], "a", "b"]),
             (
-                {"b": 2, "a": ["d", "c", {"b": {"f": 3, "e": 4}, "a": 1}]},
-                {"a": ["c", "d", {"a": 1, "b": {"f": 3, "e": 4}}], "b": 2},
+                    {"b": 2, "a": ["d", "c", {"b": {"f": 3, "e": 4}, "a": 1}]},
+                    {"a": ["c", "d", {"a": 1, "b": {"f": 3, "e": 4}}], "b": 2},
             ),
             (
-                {
-                    "c": [
-                        "2",
-                        1,
-                        [4, 3, 9, 11, {"b": 1, "a": 2}],
-                        [5, 6, 7, 3, 2, {"1": 1, "2": 2, "3": 3, "4": 4}],
-                    ],
-                    "z": 2,
-                    1: 1,
-                },
-                {
-                    1: 1,
-                    "c": [
-                        1,
-                        "2",
-                        [11, 3, 4, 9, {"a": 2, "b": 1}],
-                        [2, 3, 5, 6, 7, {"1": 1, "2": 2, "3": 3, "4": 4}],
-                    ],
-                    "z": 2,
-                },
+                    {
+                        "c": [
+                            "2",
+                            1,
+                            [4, 3, 9, 11, {"b": 1, "a": 2}],
+                            [5, 6, 7, 3, 2, {"1": 1, "2": 2, "3": 3, "4": 4}],
+                        ],
+                        "z": 2,
+                        1: 1,
+                    },
+                    {
+                        1: 1,
+                        "c": [
+                            1,
+                            "2",
+                            [11, 3, 4, 9, {"a": 2, "b": 1}],
+                            [2, 3, 5, 6, 7, {"1": 1, "2": 2, "3": 3, "4": 4}],
+                        ],
+                        "z": 2,
+                    },
             ),
         ],
     )
@@ -124,14 +123,175 @@ class TestDicts:
         "base_dct, expected_result",
         [
             (
-                [
-                    {"offset": "+0", "limit": 100, "filter": {"clientid": [132]}},
-                    {"offset": "+0", "limit": 100, "filter": {"clientid": [132]}},
-                ],
-                [{"filter": {"clientid": [132]}, "limit": 100, "offset": "+0"}],
+                    [
+                        {"offset": "+0", "limit": 100, "filter": {"clientid": [132]}},
+                        {"offset": "+0", "limit": 100, "filter": {"clientid": [132]}},
+                    ],
+                    [{"filter": {"clientid": [132]}, "limit": 100, "offset": "+0"}],
             )
         ],
     )
     def test_make_dictionary_items_unique(self, base_dct, expected_result):
         base_lst = make_dictionary_items_unique(base_dct)
         assert str(base_lst) == str(expected_result)
+
+    @pytest.mark.parametrize(
+        "base_dict, fuzzy, expected_result",
+        [
+            ({fuzzy: "1", "2": 2, "3": 3}, fuzzy, ([fuzzy.get_itself()], 0),),
+            ({"1": {fuzzy: 1}, "2": 2, "3": 3}, fuzzy, (["1", fuzzy.get_itself()], 0),),
+            (
+                    {"1": {"4": [{fuzzy: 1}]}, "2": 2, "3": 3},
+                    fuzzy,
+                    (["1", "4", 0, fuzzy.get_itself()], 0),
+            ),
+            (
+                    {"1": {"4": [{"5": [{fuzzy: 6}]}]}, "2": 2, "3": 3},
+                    fuzzy,
+                    (["1", "4", 0, "5", 0, fuzzy.get_itself()], 0),
+            ),
+        ],
+    )
+    def test_find_key_in_dlcontainer(self, base_dict, fuzzy, expected_result):
+        dl_containner = DLContainer(base_dict)
+        res, key_or_val = dl_containner.get(fuzzy, None)
+        assert (res, key_or_val) == expected_result
+
+    @pytest.mark.parametrize(
+        "base_dict, fuzzy, new_value, expected_result",
+        [
+            (
+                    {4: {fuzzy: "1", "2": 2, "3": 3}},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({4: {'7a172f8d-6f6f-4f85-af3c-8ef24a871ec4': "1", "2": 2, "3": 3}}, 0),
+            ),
+
+            (
+                    {4: {fuzzy: "1", "2": 2, "3": 3}},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({4: {'7a172f8d-6f6f-4f85-af3c-8ef24a871ec4': "1", "2": 2, "3": 3}}, 0),
+            ),
+            (
+                    {"1": {fuzzy: 1}, "2": 2, "3": 3},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({'1': {'7a172f8d-6f6f-4f85-af3c-8ef24a871ec4': 1}, '2': 2, '3': 3}, 0),
+            ),
+            (
+                    {"1": {"4": [{fuzzy: 1}]}, "2": 2, "3": 3},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({'1': {'4': [{'7a172f8d-6f6f-4f85-af3c-8ef24a871ec4': 1}]}, '2': 2, '3': 3}, 0),
+            ),
+            (
+                    {"1": {"4": [{"5": [{fuzzy: 6}]}]}, "2": 2, "3": 3},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({'1': {'4': [{'5': [{'7a172f8d-6f6f-4f85-af3c-8ef24a871ec4': 6}]}]}, '2': 2, '3': 3}, 0),
+            ),
+        ],
+    )
+    def test_find_and_update_key_in_dlcontainer(self, base_dict, fuzzy, new_value, expected_result):
+        dl_containner = DLContainer(base_dict)
+        path_to_item, key_or_value = dl_containner.get(fuzzy, None)
+        dl_containner.update_item(path_to_item, new_value, key_or_value)
+        assert (dl_containner.base_object, key_or_value) == expected_result
+
+    @pytest.mark.parametrize(
+        "base_dict, fuzzy, expected_result",
+        [
+            ({"1": fuzzy, "2": 2, "3": 3}, fuzzy, (["1"], 1)),
+            ({"1": {1: fuzzy}, "2": 2, "3": 3}, fuzzy, (["1", 1], 1)),
+            ({"1": {"4": [{1: fuzzy}]}, "2": 2, "3": 3}, fuzzy, (["1", "4", 0, 1], 1)),
+            (
+                    {"1": {"4": [{"5": [{6: fuzzy}]}]}, "2": 2, "3": 3},
+                    fuzzy,
+                    (["1", "4", 0, "5", 0, 6], 1)
+            ),
+            (
+                    {'1': [fuzzy, 'other_str']},
+                    fuzzy,
+                    (['1', 0], 1)
+            ),
+            (
+                    {'1': [[fuzzy, 'other_str']]},
+                    fuzzy,
+                    (['1', 0, 0], 1)
+            ),
+            (
+                    {'1': {2: [fuzzy, 'other_str']}},
+                    fuzzy,
+                    (['1', 2, 0], 1)
+            ),
+            (
+                    {'1': {2: [fuzzy, 'other_str']}},
+                    "fuzzy",
+                    (None, None)
+            ),
+        ],
+    )
+    def test_find_value_in_dlcontainer(self, base_dict, fuzzy, expected_result):
+        dl_containner = DLContainer(base_dict)
+        res, key_or_value = dl_containner.get(fuzzy, None)
+        assert (res, key_or_value) == expected_result
+
+    @pytest.mark.parametrize(
+        "base_dict, fuzzy, new_value, expected_result",
+        [
+            (
+                    {"1": fuzzy, "2": 2, "3": 3},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({'1': '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4', '2': 2, '3': 3}, 1)
+            ),
+            (
+                    {"1": {1: fuzzy}, "2": 2, "3": 3},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({'1': {1: '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4'}, '2': 2, '3': 3}, 1)
+            ),
+            (
+                    {"1": {"4": [{1: fuzzy}]}, "2": 2, "3": 3},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({"1": {"4": [{1: '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4'}]}, "2": 2, "3": 3}, 1)
+            ),
+            (
+                    {"1": {"4": [{"5": [{6: fuzzy}]}]}, "2": 2, "3": 3},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({"1": {"4": [{"5": [{6: '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4'}]}]}, "2": 2, "3": 3}, 1),
+            ),
+            (
+                    {'1': [fuzzy, 'other_str']},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({'1': ['7a172f8d-6f6f-4f85-af3c-8ef24a871ec4', 'other_str']}, 1)
+            ),
+            (
+                    {'1': [[fuzzy, 'other_str']]},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({'1': [['7a172f8d-6f6f-4f85-af3c-8ef24a871ec4', 'other_str']]}, 1)
+            ),
+            (
+                    {'1': {2: [fuzzy, 'other_str']}},
+                    fuzzy,
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({'1': {2: ['7a172f8d-6f6f-4f85-af3c-8ef24a871ec4', 'other_str']}}, 1)
+            ),
+            (
+                    {'1': {2: [fuzzy, 'other_str']}},
+                    "fuzzy",
+                    '7a172f8d-6f6f-4f85-af3c-8ef24a871ec4',
+                    ({'1': {2: [fuzzy, 'other_str']}}, None)
+            ),
+        ],
+    )
+    def test_find_and_update_value_in_dlcontainer(self, base_dict, fuzzy, new_value, expected_result):
+        dl_containner = DLContainer(base_dict)
+        path_to_item, key_or_value = dl_containner.get(fuzzy, None)
+        dl_containner.update_item(path_to_item, new_value, key_or_value)
+        assert (dl_containner.base_object, key_or_value) == expected_result
